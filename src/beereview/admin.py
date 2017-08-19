c8291a3783a9c19c3f3363f59c1c232d3f7aa099
snuchat <snuchat@gmail.com>
2017-08-20 01:37:34 +0900

    기초적인 맥주모델을 생성하고 이를 관리자 페이지에서 관리할 수 있음 shell을 이용하여 맥주 관련 DB를 만들어서 테스트하기 위해서 txt파일을 생성함

--- src/beereview/admin.py
+++ src/beereview/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Beer, BeerType, Production_Company
 # Register your models here.
+admin.site.register(Beer)
+admin.site.register(BeerType)
+admin.site.register(Production_Company)
