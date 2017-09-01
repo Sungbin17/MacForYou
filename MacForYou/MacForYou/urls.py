e4f8efc8f6fa21eb937eeade5fe50dff88581537
snuchat <snuchat@gmail.com>
2017-09-01 18:54:43 +0900

    beereview detail template 관련 update image 와 card관련 내용 update

--- MacForYou/MacForYou/urls.py
+++ MacForYou/MacForYou/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url, include
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 
 from beereview.views import BeerDetailView, BeerTypeDetailView
 
@@ -27,4 +29,6 @@ urlpatterns = [
     url(r'^beertype/(?P<slug>[\w-]+)/$',BeerTypeDetailView.as_view()),
     url(r'^accounts/', include('allauth.urls')),
     url(r'^community/', include('community.urls')),
-]
+] 
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
