5706acd58dee17c62c724f1542283d6bfd9c3c59
Sungbin17 <wd1kr1@gmail.com>
2017-09-05 07:20:12 +0900

    login

--- MacForYou/community/models.py
+++ MacForYou/community/models.py
@@ -1,28 +1,35 @@
 from django.db import models
 import datetime
+from django.contrib.auth.models import User
 from django.db import models
 from django.utils import timezone
+from django.conf import settings
 
 
 class Party(models.Model):
-    title = models.CharField(max_length=200)
-    content = models.TextField()
-    preferred_beer = models.CharField(max_length=200)
-    place = models.CharField(max_length=200)
-    date_meeting = models.CharField(max_length=200)
-    pub_date = models.CharField(max_length=200)
+    title= models.CharField(max_length=200)
+    content= models.TextField()
+    preferred_beer= models.CharField(max_length=200)
+    place= models.CharField(max_length=200)
+    date_meeting= models.CharField(max_length=200)
+    pub_date= models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+    likes_user_set = models.ManyToManyField(settings.AUTH_USER_MODEL,
+                                            blank=True,
+                                            related_name='likes_user_set',
+                                            through='Likes')
+
 
     def __str__(self):
         return self.title
-
+        
     def was_published_recently(self):
         now = timezone.now()
         return now - datetime.timedelta(days=1) <= self.pub_date <= now
-
     was_published_recently.admin_order_field = 'pub_date'
     was_published_recently.boolean = True
     was_published_recently.short_description = 'Published recently?'
-
 
 class Choice(models.Model):
     party = models.ForeignKey(Party, on_delete=models.CASCADE)
@@ -31,3 +38,9 @@ class Choice(models.Model):
 
     def __str__(self):
         return self.choice_text
+
+class Likes(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    party = models.ForeignKey(Party)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
